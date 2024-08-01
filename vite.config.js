66bf200d15f787c9975477e1a8b92d2c70f2fcdf
MAC <mac@MACs-MacBook-Pro.local>
2024-08-01 23:43:20 +0100

    made changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG", "**/*.PNG", "**/*.webp"],
-  base: "/"
+  base: "/klema-portfolio/"
 });
